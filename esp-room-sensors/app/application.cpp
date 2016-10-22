6a405822c00d230e4580f7771919c091e941d015
Greg V <greg@unrelenting.technology>
2016-10-22 22:17:39 +0000

    more sensors

--- esp-room-sensors/app/application.cpp
+++ esp-room-sensors/app/application.cpp
@@ -1,40 +1,84 @@
 #include <user_config.h>
 #include <SmingCore/SmingCore.h>
+#include <SmingCore/Wire.h>
 #include <Libraries/DHT/DHT.h>
+#include <Libraries/BMP180/BMP180.h>
 #include <esphandler.hpp>
 #include <wifi.h>
 
-#define DHT_PIN 0 // GPIO0
+#define DHT_PIN 4   // GPIO4  // D2
+#define PIR_PIN 5   // GPIO5  // D1
+#define DEBUG_PINS \
+	for (int i = 0; i < 16; i++) debugf("PIN %d %d", i, digitalRead(i));
 
 DHT dht(DHT_PIN, DHT22);
+BMP180 bmp;
 CoapServer srv;
 
 void handle_dht(CoapReqCtx &ctx, char **captures, size_t ncaptures) {
 	char payload[128] = { 0 };
-	if (ctx.req->getCode() == CoapPDU::COAP_GET) {
-		TempAndHumidity th;
-		if (dht.readTempAndHumidity(th)) {
-			ctx.resp->setCode(CoapPDU::COAP_CONTENT);
-			ctx.resp->setContentFormat(CoapPDU::COAP_CONTENT_FORMAT_APP_JSON);
-			StaticJsonBuffer<128> jbuf;
-			JsonObject& json = jbuf.createObject();
-			json["temperature-c"] = th.temp;
-			json["humidity-percent"] = th.humid;
-			json.printTo(payload, sizeof(payload));
-			ctx.resp->setPayload((uint8_t*)&payload, strlen(payload));
-		} else {
-			ctx.resp->setCode(CoapPDU::COAP_INTERNAL_SERVER_ERROR);
-			debugf("DHT error %d", (int)dht.getLastError());
+	if (ctx.req->getCode() != CoapPDU::COAP_GET) return;
+	TempAndHumidity th;
+	if (dht.readTempAndHumidity(th)) {
+		ctx.resp->setCode(CoapPDU::COAP_CONTENT);
+		ctx.resp->setContentFormat(CoapPDU::COAP_CONTENT_FORMAT_APP_JSON);
+		StaticJsonBuffer<128> jbuf;
+		JsonObject& json = jbuf.createObject();
+		json["temperature-c"] = th.temp;
+		json["humidity-percent"] = th.humid;
+		json.printTo(payload, sizeof(payload));
+		ctx.resp->setPayload((uint8_t*)&payload, strlen(payload));
+	} else {
+		ctx.resp->setCode(CoapPDU::COAP_INTERNAL_SERVER_ERROR);
+		debugf("DHT error %d", (int)dht.getLastError());
+	}
+}
+
+void handle_bmp(CoapReqCtx &ctx, char **captures, size_t ncaptures) {
+	char payload[128] = { 0 };
+	if (ctx.req->getCode() != CoapPDU::COAP_GET) return;
+	if (bmp.IsConnected) {
+		ctx.resp->setCode(CoapPDU::COAP_CONTENT);
+		ctx.resp->setContentFormat(CoapPDU::COAP_CONTENT_FORMAT_APP_JSON);
+		StaticJsonBuffer<128> jbuf;
+		JsonObject& json = jbuf.createObject();
+		json["pressure-pa"] = bmp.GetPressure();
+		json["temperature-c"] = bmp.GetTemperature();
+		json.printTo(payload, sizeof(payload));
+		ctx.resp->setPayload((uint8_t*)&payload, strlen(payload));
+	} else {
+		ctx.resp->setCode(CoapPDU::COAP_INTERNAL_SERVER_ERROR);
+		debugf("BMP not connected");
+		if (bmp.EnsureConnected()) {
+			bmp.SoftReset();
+			bmp.Initialize();
 		}
 	}
 }
 
+void handle_pir(CoapReqCtx &ctx, char **captures, size_t ncaptures) {
+	char payload[128] = { 0 };
+	if (ctx.req->getCode() != CoapPDU::COAP_GET) return;
+	ctx.resp->setCode(CoapPDU::COAP_CONTENT);
+	ctx.resp->setContentFormat(CoapPDU::COAP_CONTENT_FORMAT_APP_JSON);
+	StaticJsonBuffer<128> jbuf;
+	JsonObject& json = jbuf.createObject();
+	json["presence"] = digitalRead(PIR_PIN) == HIGH;
+	json.printTo(payload, sizeof(payload));
+	ctx.resp->setPayload((uint8_t*)&payload, strlen(payload));
+}
+
 void handle_core(CoapReqCtx &ctx, char **captures, size_t ncaptures) {
 	if (ctx.req->getCode() == CoapPDU::COAP_GET) {
-		char *payload = (char*)"</sensors/temp>;if=\"sensor\";rt=\"temperature-c humidity-percent\"";
+		constexpr char *payload = (char*)(
+				 "</sensors/dht>;if=\"sensor\";rt=\"temperature-c humidity-percent\""
+				",</sensors/bmp>;if=\"sensor\";rt=\"pressure-pa temperature-c\""
+				",</sensors/pir>;if=\"sensor\";rt=\"presence\""
+		);
+		constexpr size_t payload_len = strlen(payload);
 		ctx.resp->setCode(CoapPDU::COAP_CONTENT);
 		ctx.resp->setContentFormat(CoapPDU::COAP_CONTENT_FORMAT_APP_LINK);
-		ctx.resp->setPayload((uint8_t*)payload, 63);
+		ctx.resp->setPayload((uint8_t*)payload, payload_len);
 	}
 }
 
@@ -55,7 +99,9 @@ void on_receive(UdpConnection& connection, char *data, int size, IPAddress remot
 void on_connected() {
 	srv.routes.not_found = handle_not_found;
 	srv.routes / ".well-known" / "core" = handle_core;
-	srv.routes / "sensors" / "temp" = handle_dht;
+	srv.routes / "sensors" / "dht" = handle_dht;
+	srv.routes / "sensors" / "bmp" = handle_bmp;
+	srv.routes / "sensors" / "pir" = handle_pir;
 	srv.routes.debugPrint();
 	udp.listen(5683);
 	Serial.println("Server started");
@@ -68,6 +114,12 @@ void init() {
 	Serial.systemDebugOutput(true);
 	delay(1200);
 	dht.begin();
+	Wire.begin();
+	bmp = BMP180();
+  if (bmp.EnsureConnected()) {
+    bmp.SoftReset();
+    bmp.Initialize();
+	}
 	WifiStation.enable(true, false);
 	WifiStation.config(WIFI_SSID, WIFI_PASS);
 	WifiStation.waitConnection(on_connected);
